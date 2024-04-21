7509d5510c1c4a743c591401e349812493e19dbe
조우석 <dntjrwh@gmail.com>
2024-04-21 19:08:57 +0900

    채팅 수정 중 #3

--- pt-ide/src/components/ChatHeader.tsx
+++ pt-ide/src/components/ChatHeader.tsx
@@ -1,18 +1,26 @@
 import React, {useState} from 'react';
 import CreateRoomModal from './CreateRoomModal';
 
-interface ChatHeaderProps {
-  onOpenModal: () => void; // 모달을 여는 함수를 props로 받습니다.
-}
+const ChatHeader: React.FC = () => {
 
-const ChatHeader: React.FC<ChatHeaderProps> = ({ onOpenModal }) => {
+  const [isModalOpen, setModalOpen] = useState<boolean>(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
 
   return(
     <div className="flex flex-row items-center justify-end">
-      <div className='mr-4 cursor-pointer' onClick={onOpenModal}>
+      <div className='mr-4 cursor-pointer' onClick={handleOpenModal}>
         <svg className="w-8 h-8 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
           <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4.243a1 1 0 1 0-2 0V11H7.757a1 1 0 1 0 0 2H11v3.243a1 1 0 1 0 2 0V13h3.243a1 1 0 1 0 0-2H13V7.757Z" clip-rule="evenodd"/>
         </svg>
+        {isModalOpen && <CreateRoomModal isOpen={isModalOpen} onClose={handleCloseModal} />}
       </div>
       <div className='mr-4 cursor-pointer'>
         <svg className="w-8 h-8 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
--- pt-ide/src/components/Chatting.tsx
+++ pt-ide/src/components/Chatting.tsx
@@ -1,41 +1,14 @@
-import React,{useState} from 'react';
-import MessageList from './MessageList';  // 메시지 리스트 컴포넌트
-import ChatBar from './ChatBar';          // 입력바 컴포넌트
-import ChatHeader from './ChatHeader';
-import CreateRoomModal from './CreateRoomModal';
-import ChatRoomList from './ChatRoomList';
-//import ChatRoomList from './ChatRoomList';  
+import React from "react";
+import ChatHeader from "./ChatHeader";
+import CreateRoomModal from "./CreateRoomModal";
 
-interface Room {
-  id: number;
-  name: string;
-}// 채팅 창 컴포넌트
-
-const Chatting: React.FC = () => {
-  const [rooms, setRooms] = useState<Room[]>([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleCreateRoom = (roomName: string) => {
-    const newRoom: Room = {
-      id: rooms.length + 1, // 간단한 ID 할당 방식입니다.
-      name: roomName,
-    };
-    setRooms([...rooms, newRoom]);
-    setIsModalOpen(false); // 방을 생성한 후 모달을 닫습니다.
-  };
-
-  return (
+const Chatting = () =>{
+  return(
     <div>
-    <ChatHeader onOpenModal={() => setIsModalOpen(true)} />
-    {/* <ChatRoomList rooms={rooms} /> */}
-    <CreateRoomModal
-      isOpen={isModalOpen}
-      onClose={() => setIsModalOpen(false)}
-      onCreateRoom={handleCreateRoom}
-    />
-  </div>
-  );
-};
+      <ChatHeader/>
+      
+    </div>
+  )
+}
 
 export default Chatting;
-
