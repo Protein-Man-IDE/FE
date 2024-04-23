cbca197e5c25c5c126ef269796fa569a42335caa
Danbaekman <gostart5@naver.com>
2024-04-23 16:14:26 +0900

    로그인 화면 디자인 #5

--- pt-ide/src/pages/login/Login.tsx
+++ pt-ide/src/pages/login/Login.tsx
@@ -1,47 +1,52 @@
-import React,{useState} from 'react';
-import {Link} from 'react-router-dom';
-
-// 아이디 비밀번호 닉네임 이메일
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    id: '',
+    password: '',
+  });
 
-  const [formData, setformData] = useState({
-    id:'',
-    password:'',
-});
-
-return (
-  <div className="flex items-center justify-center h-screen bg-gray-100 dark:bg-gray-900 overflow-hidden">
-    <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-      <form className="space-y-6" action="#">
-        <h5 className="flex justify-center text-xl font-medium text-gray-900 dark:text-white">로그인</h5>
-          <div className="relative z-0 w-full mb-5 group">
-            <input type="text" name="repeat_password" id="floating_repeat_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-            <label htmlFor="floating_repeat_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">아이디</label>
-          </div>
-          <div className="relative z-0 w-full mb-5 group">
-            <input type="password" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-            <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">비밀번호</label>
-          </div>
-          <div className="flex items-start">
+  return (
+    <div className="relative h-[100vh] items-center justify-center bg-white dark:bg-black">
+      <div className="absolute inset-0 overflow-hidden">
+        <div className="jumbo absolute -inset-[10px] opacity-50" />
+      </div>
+      <div className="flex items-center justify-center h-screen bg-gray-100 dark:bg-gray-900 overflow-hidden">
+        <div className="relative w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-600 border-1"> {/* 수정된 부분 */}
+          <form className="space-y-6" action="#">
+            <h5 className="text-bold flex justify-center text-xl font-medium text-gray-900 dark:text-white">Sign in</h5>
+            <div className="relative z-0 w-full mb-5 group">
+              <input type="text" name="repeat_password" id="floating_repeat_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+              <label htmlFor="floating_repeat_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:--translate-y-6">Id</label>
+            </div>
+            <div className="relative z-0 w-full mb-5 group">
+              <input type="password" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+              <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+            </div>
             <div className="flex items-start">
-              <div className="flex items-center h-5">
-                <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+              <div className="flex items-start">
+                <div className="flex items-center h-5">
+                  <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+                </div>
+                <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember Me</label>
               </div>
-                <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">아이디 저장</label>
+              <Link to="/forgotpassword" className="ms-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Forgot Password?</Link>
             </div>
-              <Link to="/forgotpassword" className="ms-auto text-sm text-blue-700 hover:underline dark:text-blue-500">비밀번호를 잊으셨나요?</Link>
-          </div>
-          <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">로그인</button>
+            <button
+              type="submit"
+              className="relative w-full text-white bg-gradient-to-r from-red-500 via-orange-500 to-yellow-500 to-green-500 to-blue-500 to-indigo-500 to-violet-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center transition duration-300 ease-in-out"
+            >
+              Log in
+            </button>
             <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-              회원이 아니신가요? <Link to="/signup" className="text-blue-700 hover:underline dark:text-blue-500">회원가입</Link>
+              Need an account?<Link to="/signup" className="text-blue-700 hover:underline dark:text-blue-500">   Create an account</Link>
             </div>
-        </form>
+          </form>
+        </div>
+      </div>
     </div>
-  </div>
-
-    );
-
-}
+  );
+};
 
 export default Login;
